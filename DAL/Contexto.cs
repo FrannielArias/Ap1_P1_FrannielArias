42d98f94fefc9f00d301d0cc2e0c7a73d9a7deda
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-06-03 18:39:02 -0400

    Agregando el contexto a Articulos

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ap1_P1_FrannielArias.Modelss;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ap1_P1_FrannielArias.DAL
 {
     public class Contexto : DbContext
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Articulos> Articulos { get; set; }
     }
 }
