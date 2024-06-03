c9dd213a3c8172274bfd4c7bd612d16dade15d2c
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-06-03 18:34:14 -0400

    Creacion de los componentes

---
+++ Modelss/Articulos.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ap1_P1_FrannielArias.Modelss
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar una descripcion.")]
+        public string? Descripcion { get; set; }
+
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El costo debe ser mayor a 0 o es superior al soportado por el sistema")]
+        [Range(minimum:0.1, maximum:99999999999999)]
+        public Decimal Costo { get; set; }
+
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El porcentaje de ganancia debe ser mayor a 0 o es superior al soportado por el sistema")]
+        [Range(minimum: 0.1, maximum: 99999999999999)]
+        public Decimal Ganancia { get; set;}
+
+        [RegularExpression("^[0-9]+$", ErrorMessage = "El precio de venta debe ser mayor a 0 o es superior al soportado por el sistema")]
+        [Range(minimum: 0.1, maximum: 99999999999999)]
+        public Decimal Venta {  get; set; }
+        
+    }
+}
