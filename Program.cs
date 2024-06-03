eba3341bb501c4882ef4808b33d30ce2f9f3fa17
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-06-03 17:30:50 -0400

    Inyección de la base de datos en el program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using Ap1_P1_FrannielArias.Components;
+using Ap1_P1_FrannielArias.DAL;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContextFactory<Contexto>(options => options.UseSqlite(ConStr));
 
 var app = builder.Build();
 
